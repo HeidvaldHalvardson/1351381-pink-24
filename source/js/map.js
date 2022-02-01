ad7d2b2628e4a33b8e743a986fcfabf98211249f
Vlad Bogodyazh <vladbg53@gmail.com>
2022-02-01 20:44:11 +0300

    Исправления по защите

---
+++ source/js/map.js
@@ -0,0 +1,5 @@
+var mapImage = document.querySelector('.map__image');
+var mapFrame = document.querySelector('.map__frame');
+
+mapImage.classList.remove('map__image--nojs');
+mapFrame.classList.remove('map__frame--nojs');
